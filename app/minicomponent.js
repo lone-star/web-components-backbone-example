f9b227bcb3dfc691a2c34c1454ecd57e5dc7c5ac
benjamin <benjamin@justpark.com>
2015-01-01 16:01:38 +0000

    enable the inclusion of MiniComponent as AMD, CommonJS, or in-browser

--- app/minicomponent.js
+++ app/minicomponent.js
@@ -1,128 +1,151 @@
 
-window.MiniComponent = function(appName){
-  var _this = this;
-  this.services = {};
+(function(root, factory) {
 
-  document.registerElement(appName, {
-    prototype: _.extend(Object.create(HTMLElement.prototype), {
-      createdCallback: function() {
-        _this.el = this;
-      }
-    })
-  });
-};
+  if (typeof define === 'function' && define.amd) {
 
-MiniComponent.prototype = {
-  registerView: function(name, View) {
+    define(['underscore'], function(_){
+      root.MiniComponent = factory(root, _);
+      return root.MiniComponent;
+    });
+
+  } else if (typeof exports != 'undefined') {
+
+    var _ = require('underscore');
+    module.exports = factory(root, _);
+
+  } else {
+    root.MiniComponent = factory(root, root._);
+  }
+
+})(this, function(root, _) {
+
+  var MiniComponent = function(appName){
     var _this = this;
+    this.services = {};
 
-    document.registerElement(name, {
+    document.registerElement(appName, {
       prototype: _.extend(Object.create(HTMLElement.prototype), {
         createdCallback: function() {
-          var attributes = this.attributes;
-          var namedItem;
-          var options = {};
-
-          _.each(_this.services, function(service, key){
-            if (attributes.getNamedItem(key)) {
-              options[key] = service;
-            }
-          });
-
-          if (namedItem = attributes.getNamedItem('collection')) {
-            options[namedItem.value] = _this.services[namedItem.value];
-            options['collection'] = _this.services[namedItem.value];
-          }
-
-          if (namedItem = attributes.getNamedItem('collection')) {
-            options[namedItem.value] = _this.services[namedItem.value];
-            options['model'] = _this.services[namedItem.value];
-          }
-
-          view = new View(_.extend(options, {
-            el: this,
-          }));
-
-          _this.replaceChildrenNode(view, this.innerHTML);
-
-          _this.parseListeners(attributes, view, options);
+          _this.el = this;
         }
       })
     });
-  },
+  };
 
-  replaceChildrenNode: function(view, children) {
+  MiniComponent.prototype = {
+    registerView: function(name, View) {
+      var _this = this;
 
-    if (children.length > 0) {
-      var render = view.render;
+      document.registerElement(name, {
+        prototype: _.extend(Object.create(HTMLElement.prototype), {
+          createdCallback: function() {
+            var attributes = this.attributes;
+            var namedItem;
+            var options = {};
 
-      view.render = function() {
+            _.each(_this.services, function(service, key){
+              if (attributes.getNamedItem(key)) {
+                options[key] = service;
+              }
+            });
 
-        var element;
-        var parentNode;
-        var newElement;
+            if (namedItem = attributes.getNamedItem('collection')) {
+              options[namedItem.value] = _this.services[namedItem.value];
+              options['collection'] = _this.services[namedItem.value];
+            }
 
-        render.apply(view);
-        element = document.getElementsByTagName('mini-children')[0];
-        parentNode = element.parentNode;
+            if (namedItem = attributes.getNamedItem('collection')) {
+              options[namedItem.value] = _this.services[namedItem.value];
+              options['model'] = _this.services[namedItem.value];
+            }
 
-        setTimeout(function(){
-          newElement = document.createElement('div');
-          newElement.innerHTML = children;
-          parentNode.replaceChild(newElement, element);
-        });
-      };
+            view = new View(_.extend(options, {
+              el: this,
+            }));
 
-      view.render();
+            _this.replaceChildrenNode(view, this.innerHTML);
 
-    } else {
-      view.render();
-    }
-  },
+            _this.parseListeners(attributes, view, options);
+          }
+        })
+      });
+    },
 
-  parseListeners: function(attributes, view, options) {
-    var attributesArray = Array.prototype.slice.call(attributes);
-    var node;
-    var method;
+    replaceChildrenNode: function(view, children) {
 
-    for(key in attributesArray) {
-      node = attributesArray[key];
+      if (children.length > 0) {
+        var render = view.render;
 
-      if (node.nodeName.match(/-on$/)) {
-        method = node.nodeName.replace(/-on$/, '');
+        view.render = function() {
 
-        this.registerMethod(method, node.value, view, options);
+          var element;
+          var parentNode;
+          var newElement;
+
+          render.apply(view);
+          element = document.getElementsByTagName('mini-children')[0];
+          parentNode = element.parentNode;
+
+          setTimeout(function(){
+            newElement = document.createElement('div');
+            newElement.innerHTML = children;
+            parentNode.replaceChild(newElement, element);
+          });
+        };
+
+        view.render();
+
+      } else {
+        view.render();
       }
+    },
+
+    parseListeners: function(attributes, view, options) {
+      var attributesArray = Array.prototype.slice.call(attributes);
+      var node;
+      var method;
+
+      for(key in attributesArray) {
+        node = attributesArray[key];
+
+        if (node.nodeName.match(/-on$/)) {
+          method = node.nodeName.replace(/-on$/, '');
+
+          this.registerMethod(method, node.value, view, options);
+        }
+      }
+    },
+
+    registerMethod: function(method, eventsDefinition, view, options) {
+      var eventArray = eventsDefinition.split(' ');
+      var eventDefinition;
+      var service;
+      var callers;
+
+      for(key in eventArray) {
+        eventDefinition = eventArray[key];
+
+        service = this.getServiceFromEventDefinition(eventDefinition, options);
+        callers = this.getCallersFromEventDefinition(eventDefinition);
+
+        view.listenTo(service, callers, view[method]);
+      }
+    },
+
+    getServiceFromEventDefinition: function(eventDefinition, options) {
+      var serviceName = eventDefinition.replace(/\|(.+)$/, '')
+      return options[serviceName];
+    },
+
+    getCallersFromEventDefinition: function(eventDefinition) {
+      var callers = eventDefinition.replace(/^.*\|/, '')
+      return callers.replace(/,/g, ' ');
+    },
+
+    registerService: function(name, collection) {
+      this.services[name] = collection;
     }
-  },
+  };
 
-  registerMethod: function(method, eventsDefinition, view, options) {
-    var eventArray = eventsDefinition.split(' ');
-    var eventDefinition;
-    var service;
-    var callers;
-
-    for(key in eventArray) {
-      eventDefinition = eventArray[key];
-
-      service = this.getServiceFromEventDefinition(eventDefinition, options);
-      callers = this.getCallersFromEventDefinition(eventDefinition);
-
-      view.listenTo(service, callers, view[method]);
-    }
-  },
-
-  getServiceFromEventDefinition: function(eventDefinition, options) {
-    var serviceName = eventDefinition.replace(/\|(.+)$/, '')
-    return options[serviceName];
-  },
-
-  getCallersFromEventDefinition: function(eventDefinition) {
-    var callers = eventDefinition.replace(/^.*\|/, '')
-    return callers.replace(/,/g, ' ');
-  },
-
-  registerService: function(name, collection) {
-    this.services[name] = collection;
-  }
-};
+  return MiniComponent;
+});
